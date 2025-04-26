9fe1bb4f1ae4f875d367b44ae44caa28d248fd31
KIMGITACHI0916 <148501410+KIMGITACHI0916@users.noreply.github.com>
2025-04-26 12:55:20 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,10 @@
 from telegram.ext import ApplicationBuilder, CommandHandler
 
-async def start(update, context):
-    await update.message.reply_text('Hello! I am alive!')
-
 app = ApplicationBuilder().token("7786341898:AAHdPjculC44KfjYmVyjvbloEgkfaCmkGwE").build()
+
+# Example: /start command
+async def start(update, context):
+    await update.message.reply_text("Hello, I'm alive!")
 
 app.add_handler(CommandHandler("start", start))
 
